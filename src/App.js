a964ebe422fdb33996d93cf2d5e6d208c074de0b
Marco Antonio Trujano Rodriguez <trujas_2309@hotmail.com>
2020-11-19 20:13:47 -0600

    cambios mios

--- src/App.js
+++ src/App.js
@@ -37,13 +37,27 @@ export default class App extends React.Component {
             })
     }
 
+    getNumPokemon = (index) => {
+      index++
+      let numPokemon = ''
+      if (index < 10){
+        numPokemon = `00${index}`
+      }else if(index < 100){
+        numPokemon = `0${index}`
+      }else if(index < 1000){
+        numPokemon = `${index}`
+      }else{
+        console.log('muchos pokemons')
+      }  
+      return numPokemon
+    }
+
     render() {
         return (
             <div className="pokedex-container">
               {
-                this.state.pokemones.map( (pokemon, index) => {      
-                  //2. Solucionar el problema de obtener las imagenes de los pokemones con id < 10, > 10, > 100            
-                  let pokemonImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${index + 1}.png`
+                this.state.pokemones.map( (pokemon, index) => {  
+                  let pokemonImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.getNumPokemon(index)}.png`
                   return (
                     <Card key={index + 1} name={pokemon.name} img={pokemonImg} />
                   )
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -5,11 +5,22 @@ function Pagination(props){
     //3. Mostrar las páginas disponibles
     //Si solicitamos la página 10 mostrariamos [5, 6, 7, 8, 9, 10, 11, 12, 13, 14] ( |10 - 5| |10 + 4| )
     //Si solicitamos la página 14 mostrariamos [9, 10, 11, 12, 13, 14, 15, 16, 17, 18] ( |14 - 5 | |14 + 4| )
+    let getNumButton = (e) => {
+        console.log(e.target.value)
+    }
     return (
         <div className="pagination-row">
             {
                 pokemonArray.map( (element, index) => {
-                    return <div className="item" onClick={() => props.fetchPageFn(2)}>{index + 1}</div>
+                    return  <div 
+                                key={index}
+                                onClick={() => props.fetchPageFn(3)}
+                                onClick={(evento) => getNumButton(evento)}
+                                className="item">
+                                {index+1}
+                            </div>
+                            
+                    
                 })
             }
             
